78b374bcad8a5b9674873a739c2399d8357767a6
Javier Uruen Val <juruen@spotify.com>
2013-08-01 14:04:22 +0200

    Working version

--- stable-marriage/stable-marriage.py
+++ stable-marriage/stable-marriage.py
@@ -1,24 +1,36 @@
 #!/usr/bin/python env
 import sys
+import sets
 
-def propose(man, woman, women, marriages):
-    if woman not in marriages:
-        current_pref = sys.maxint
-    else:
-        current_pref = women[woman].index(marriages[woman])
-    if man not in women[woman]:
-        new_pref = sys.maxint
-    else:
-        new_pref = women[woman].index(man)
-    return (new_pref < current_pref)
+def next_propose(free_m, m_pref, proposed_to):
+    if not free_m:
+        return (False, (None, None))
+    for m in free_m:
+        for w in m_pref[m]:
+            if w not in proposed_to[m]:
+                return (True, (m,w))
+    return (False, (None, None))
 
-def stable_marriage(men, women):
-    marriages = {}
-    for man, prefer in men.items():
-        for woman in prefer:
-            if propose(man, woman, women, marriages):
-                marriages[woman] = man
-                break
-    print marriages
+def w_prefers_new(w, cm, nm, pref):
+    if nm not in pref:
+        return False
+    return pref.index(nm) < pref.index(cm)
 
-stable_marriage({"m1": ["w1", "w2"], "m2": ["w1", "w2"]}, {"w1": ["m1", "m2"], "w2": ["m1", "m2"]})
+def stable_matching(nm, nw, m_pref, w_pref):
+    w_engaged_to = {}
+    m_set = sets.Set(range(0, nm))
+    m_proposed_to = {i:sets.Set() for i in m_set}
+
+    while True:
+        free_m = m_set.difference(sets.Set(w_engaged_to.values()))
+        free_w = m_set.difference(w_engaged_to.keys())
+        (propose_left, (m, w)) = next_propose(free_m, m_pref, m_proposed_to)
+        if not propose_left:
+            break
+        m_proposed_to[m].add(w)
+        if w in free_w or w_prefers_new(w, w_engaged_to[w], m, w_pref[w]):
+            w_engaged_to[w] = m
+
+    print w_engaged_to
+
+stable_matching(4, 4, {0: [2, 1, 3, 0], 1: [1, 2, 0, 3], 2: [2, 0, 1, 3], 3: [2, 1, 3, 0]}, {0: [3, 2, 0, 1], 1: [1, 0, 2, 3], 2: [0, 2, 3, 1], 3: [3, 2, 0, 1]})
